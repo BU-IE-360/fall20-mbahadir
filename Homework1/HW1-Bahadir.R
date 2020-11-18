6f68302c9bedd00b088651c3528618198796e263
mbahadir <70413025+mbahadir@users.noreply.github.com>
2020-11-18 23:46:33 +0300

    Basic format changes

--- Homework1/HW1-Bahadir.R
+++ Homework1/HW1-Bahadir.R
@@ -16,10 +16,14 @@ EVDS=read.xlsx("EVDS.xlsx",sheetIndex=1, header=TRUE)
 summary(EVDS)
 str(EVDS)
 names(EVDS)
+EVDS <- EVDS[-c(255, 256), ]
 EVDS$Tarih=dmy(EVDS$Tarih)
+EVDS$Week=week(EVDS$Tarih)
 EVDS$month_year <-format(EVDS$Tarih,'%Y-%m')
+
 str(EVDS)
-ggplot(EVDS,aes(x=month_year,y=TP.DK.USD.A.EF.YTL))+ geom_bar(stat='identity')+ 
+ggplot(EVDS,aes(x=Week,y=TP.DK.USD.A.EF.YTL))+ geom_bar(stat='identity')+
+  facet_wrap(~year(Tarih))+
   xlab("Date") + ylab("Exhange Rate")+ ggtitle("Dollar Exchange Rate vs Time")
 ###############################
 unemployement_df=read.csv("unemployement.csv", stringsAsFactors = FALSE) 
@@ -27,16 +31,19 @@ summary(unemployement_df)
 str(unemployement_df)
 unemployement_df$Tarih=as.Date(as.yearmon(unemployement_df$Tarih))
 names(unemployement_df)
-ggplot(unemployement_df,aes(x=year(Tarih),y=issizlik))+ geom_bar(stat='identity')+ 
+ggplot(unemployement_df,aes(x=month(Tarih),y=issizlik))+ geom_bar(stat='identity')+ 
+  facet_wrap(~year(Tarih))+
   xlab("Years") + ylab("Percentage of Unemployement ")+ ggtitle("Unemployement in Turkey vs Time")
 ###############################
 covid_df=read.csv("covid19-Turkey.csv")
 summary(covid_df)
 str(covid_df)
 covid_df$date=as.Date(covid_df$date)
+covid_df$day=day(covid_df$date)
 covid_df$month_year <-format(covid_df$date,'%Y-%m')
 names(covid_df)
-ggplot(covid_df,aes(x=month_year,y=dailyCases))+ geom_bar(stat='identity')+ 
+ggplot(covid_df,aes(x=day,y=dailyCases))+ geom_bar(stat='identity')+ 
+  facet_wrap(~month(date))+
   xlab("Months") + ylab("Number of Covid Cases")+ ggtitle("Covid Cases in Turkey vs Time")
 ################################
 
@@ -48,10 +55,11 @@ dolar_sr=read.csv("dolar_search.csv")
 str(dolar_sr)
 dolar_sr$Date=ymd(dolar_sr$Date)
 dolar_sr$month_year <-format(dolar_sr$Date,'%Y-%m')
-ggplot(dolar_sr,aes(x=month_year,y=Search))+ geom_boxplot()+ 
+dolar_sr$Week=week(dolar_sr$Date)
+ggplot(dolar_sr,aes(x=factor(year(Date)),y=Search))+ geom_boxplot(aes(fill=factor(year(Date))))+ 
   xlab("Years") + ylab("Percentage of Unemployement ")+ ggtitle("Boxplot for Dolar Search")
 
-ggplot(EVDS,aes(x=month_year,y=TP.DK.USD.A.EF.YTL))+ geom_boxplot()+ 
+ggplot(EVDS,aes(x=factor(year(Tarih)),y=TP.DK.USD.A.EF.YTL))+ geom_boxplot(aes(fill=factor(year(Tarih))))+ 
   xlab("Date") + ylab("Exhange Rate")+ ggtitle("Boxplot for Dollar Exchange Rate")
 ################################
 unemployement_sr=read.csv("is_search.csv")
