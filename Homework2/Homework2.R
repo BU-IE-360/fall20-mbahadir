abcdff80d3f23126e1b321c11ec213a7b67cf64a
mbahadir <70413025+mbahadir@users.noreply.github.com>
2020-12-09 20:15:04 +0300

    Full Version 1

--- Homework2/Homework2.R
+++ Homework2/Homework2.R
@@ -11,6 +11,7 @@ install.packages("GGally")
 library(GGally)
 
 
+
 #Loading the datasets
 price_index=read.xlsx("Price Index.xlsx",sheetIndex=1, header=TRUE)
 plot(price_index)
@@ -32,29 +33,59 @@ str(dolar_exchange_rate)
 #Merge the datasets
 sub_df<-merge(price_index,unemployment_rate,by="Tarih")
 df<-merge(sub_df,dolar_exchange_rate,by="Tarih")
-
+df
 
 #Convert to the Time-Series
+df_ts <- xts(df)
+
 df_ts <- xts(df[,-1], order.by=as.Date(df[,1], "%Y/%m/%d"))
 glimpse(df_ts)
 
+colnames(df_ts)[1]<-"tufe_index"
+colnames(df_ts)[2]<-"unemp_rate"
+colnames(df_ts)[3]<-"dolar_rate"
+df_ts
 
-autoplot(df_ts[,c("TP.TUFE1YI.T1")])+ggtitle("Time vs Price Index(01/2017 to 08/2020)")+
+
+
+df_ts$tufe_index_normalized=(df_ts$tufe_index-min(df_ts$tufe_index))/(max(df_ts$tufe_index)-min(df_ts$tufe_index))
+df_ts$unemp_rate_normalized=(df_ts$unemp_rate-min(df_ts$unemp_rate))/(max(df_ts$unemp_rate)-min(df_ts$unemp_rate))
+df_ts$dolar_rate_normalized=(df_ts$dolar_rate -min(df_ts$dolar_rate ))/(max(df_ts$dolar_rate )-min(df_ts$dolar_rate ))
+
+cols <- c("Dolar Exchange Rate" = "red", "Tufe Index" = "yellow", "Unemployement Rate" = "blue")
+
+ggplot(df_ts)+geom_line(aes(x=Index, y=tufe_index_normalized,color="Dolar Exchange Rate"))+
+  geom_line(aes(x=Index, y=unemp_rate_normalized ,color="Tufe Index"))+
+  geom_line(aes(x=Index, y=dolar_rate_normalized , color= "Unemployement Rate"))+
+  ggtitle("Normalized Indexes Comparision")+
+  ylab("Normalized Values")+xlab("Date")+
+  scale_color_manual(values = cols)+
+  labs(colour = "Indexes")
+  
+
+
+autoplot(df_ts[,c("tufe_index")])+ggtitle("Time vs Price Index(01/2017 to 08/2020)")+
   ylab("Price Index")
 
-autoplot(df_ts[,c("TP.TIG07")])+ggtitle("Time vs Price Index(01/2017 to 08/2020)")+
+autoplot(df_ts[,c("unemp_rate")])+ggtitle("Time vs Price Index(01/2017 to 08/2020)")+
   ylab("Price Index")
 
-autoplot(df_ts[,c("TP.DK.USD.S.YTL")])+ggtitle("Time vs Price Index(01/2017 to 08/2020)")+
+autoplot(df_ts[,c("dolar_rate")])+ggtitle("Time vs Price Index(01/2017 to 08/2020)")+
   ylab("Price Index")
+
+
+
+ggplot(df_ts,)
 
 #General looking with 3 different graph
 plot(zoo(df_ts))
 
 
 
+df_ts_req=df_ts[,c(1,2,3)]
+df_ts_req
 #Correlation between the variables
-ggpairs(data.frame(df_ts))
+ggpairs(data.frame(df_ts_req))
 
 
 
