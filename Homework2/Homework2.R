ddc5a41271062b2835d2fc23e34f32283c70d817
mbahadir <70413025+mbahadir@users.noreply.github.com>
2020-12-11 02:28:29 +0300

    Indexes

--- Homework2/Homework2.R
+++ Homework2/Homework2.R
@@ -1,97 +1,104 @@
-setwd("C:/Users/bahad/GitHub/IE360/Homework2")
-install.packages("xlsx")
 library(xlsx)
-library(fpp2)
-library(ggplot2)
-library(lubridate)
-library(zoo)
-library(dplyr)
-library(xts)
-install.packages("GGally")
-library(GGally)
+library(ggplot2, quietly=TRUE)
+library(dplyr, quietly=TRUE)
+library(xts, quietly=TRUE)
+library(fpp2, quietly=TRUE)
+library(GGally, quietly=TRUE)
 
-
-
-#Loading the datasets
 price_index=read.xlsx("Price Index.xlsx",sheetIndex=1, header=TRUE)
-plot(price_index)
+#plot(price_index)
 price_index$Tarih=as.Date(as.yearmon(price_index$Tarih))
 str(price_index)
 
-unemployment_rate=read.xlsx("Unemployement Rate.xlsx",sheetIndex=1, header=TRUE)
-plot(unemployment_rate)
-unemployment_rate$Tarih=as.Date(as.yearmon(unemployment_rate$Tarih))
-str(unemployment_rate)
+property_price_index=read.xlsx("Residential Property Price Index.xlsx",sheetIndex=1, header=TRUE)
+#plot(property_price_index)
+property_price_index$Tarih=as.Date(as.yearmon(property_price_index$Tarih))
+str(property_price_index)
 
-dolar_exchange_rate=read.xlsx("Dolar Exchange Rate.xlsx",sheetIndex = 1, header=TRUE)
-plot(dolar_exchange_rate)
-dolar_exchange_rate$Tarih=as.Date(as.yearmon(dolar_exchange_rate$Tarih))
-str(dolar_exchange_rate)
+dollar_exchange_rate=read.xlsx("Dolar Exchange Rate.xlsx",sheetIndex = 1, header=TRUE)
+#plot(dollar_exchange_rate)
+dollar_exchange_rate$Tarih=as.Date(as.yearmon(dollar_exchange_rate$Tarih))
+str(dollar_exchange_rate)
 
-
-
-#Merge the datasets
-sub_df<-merge(price_index,unemployment_rate,by="Tarih")
-df<-merge(sub_df,dolar_exchange_rate,by="Tarih")
-df
-
-#Convert to the Time-Series
-df_ts <- xts(df)
+sub_df<-merge(price_index,property_price_index,by="Tarih")
+df<-merge(sub_df,dollar_exchange_rate,by="Tarih")
 
 df_ts <- xts(df[,-1], order.by=as.Date(df[,1], "%Y/%m/%d"))
 glimpse(df_ts)
 
-colnames(df_ts)[1]<-"tufe_index"
-colnames(df_ts)[2]<-"unemp_rate"
-colnames(df_ts)[3]<-"dolar_rate"
-df_ts
+colnames(df_ts)[1]<-"producer_price_index"
+colnames(df_ts)[2]<-"prop_price_index"
+colnames(df_ts)[3]<-"dollar_rate"
 
+autoplot(df_ts[,c("producer_price_index")])+ggtitle("Time vs Producer Price Index (01/2017 to 08/2020)")+
+  ylab("Producer Price Index")+xlab("Date (Monthly)")
 
+autoplot(df_ts[,c("prop_price_index")])+ggtitle("Time vs Property Price Index (01/2017 to 08/2020)")+
+  ylab("Property Price Index")+xlab("Date (Monthly)")
 
-df_ts$tufe_index_normalized=(df_ts$tufe_index-min(df_ts$tufe_index))/(max(df_ts$tufe_index)-min(df_ts$tufe_index))
-df_ts$unemp_rate_normalized=(df_ts$unemp_rate-min(df_ts$unemp_rate))/(max(df_ts$unemp_rate)-min(df_ts$unemp_rate))
-df_ts$dolar_rate_normalized=(df_ts$dolar_rate -min(df_ts$dolar_rate ))/(max(df_ts$dolar_rate )-min(df_ts$dolar_rate ))
+autoplot(df_ts[,c("dollar_rate")])+ggtitle("Time vs Dollar Index(01/2017 to 08/2020)")+
+  ylab("Dollar Currency Rate")+xlab("Date (Monthly)")#+geom_smooth()
 
-cols <- c("Dolar Exchange Rate" = "red", "Tufe Index" = "yellow", "Unemployement Rate" = "blue")
+indexes<-c("Producer Price Index","Property Price Index", "Dollar Index" )
+plot(zoo(df_ts), main="General comparsion of Indexes ", xlab="Date (monthly)",ylab=indexes)
 
-ggplot(df_ts)+geom_line(aes(x=Index, y=tufe_index_normalized,color="Dolar Exchange Rate"))+
-  geom_line(aes(x=Index, y=unemp_rate_normalized ,color="Tufe Index"))+
-  geom_line(aes(x=Index, y=dolar_rate_normalized , color= "Unemployement Rate"))+
-  ggtitle("Normalized Indexes Comparision")+
-  ylab("Normalized Values")+xlab("Date")+
+df_ts$producer_price_index_index_normalized=(df_ts$producer_price_index-min(df_ts$producer_price_index))/(max(df_ts$producer_price_index)-min(df_ts$producer_price_index))
+df_ts$prop_price_index_normalized=(df_ts$prop_price_index-min(df_ts$prop_price_index))/(max(df_ts$prop_price_index)-min(df_ts$prop_price_index))
+df_ts$dollar_rate_normalized=(df_ts$dollar_rate -min(df_ts$dollar_rate ))/(max(df_ts$dollar_rate )-min(df_ts$dollar_rate ))
+
+cols <- c("Dollar Exchange Rate" = "red", "Producer Price Index" = "yellow", "Residency Price Index" = "blue")
+
+ggplot(df_ts)+geom_line(aes(x=Index, y=producer_price_index_index_normalized,color="Dollar Exchange Rate"))+
+  geom_line(aes(x=Index, y=prop_price_index_normalized ,color="Producer Price Index"))+
+  geom_line(aes(x=Index, y=dollar_rate_normalized , color= "Residency Price Index"))+
+  ggtitle("Comparision of Normalized Indexes")+
+  ylab("Normalized Values")+xlab("Date (Monthly)")+
   scale_color_manual(values = cols)+
-  labs(colour = "Indexes")
-  
+  labs(colour = "Indexes")+ 
+    theme(
+    legend.position = c(.95, .25),
+    legend.justification = c("right", "top"),
+    legend.box.just = "right",
+    legend.margin = margin(6, 6, 6, 6)
+    )
 
+ggplot(df_ts, aes(producer_price_index))+ggtitle("Density of Producer Price Index (01/2017 to 08/2020)")+
+  ylab("Density")+xlab("Producer Price Index")+geom_density(fill="lightblue")
 
-autoplot(df_ts[,c("tufe_index")])+ggtitle("Time vs Price Index(01/2017 to 08/2020)")+
-  ylab("Price Index")
+ggplot(df_ts, aes(prop_price_index))+ggtitle("Density of Residency Price Index(01/2017 to 08/2020)")+
+  ylab("Density")+xlab("Residency Price Index")+geom_density(fill="lightblue")
 
-autoplot(df_ts[,c("unemp_rate")])+ggtitle("Time vs Price Index(01/2017 to 08/2020)")+
-  ylab("Price Index")
+ggplot(df_ts, aes(dollar_rate))+ggtitle("Density of Dollar Index(01/2017 to 08/2020)")+
+  ylab("Density")+xlab("Dollar Currency Rate")+geom_density(fill="lightblue")
 
-autoplot(df_ts[,c("dolar_rate")])+ggtitle("Time vs Price Index(01/2017 to 08/2020)")+
-  ylab("Price Index")
+ggplot() +
+  geom_density(aes(producer_price_index_index_normalized , fill = "Producer Price Index"), alpha = .2, data = df_ts) +
+  geom_density(aes(prop_price_index_normalized  , fill = "Residency Price Index"), , alpha = .2, data = df_ts) +
+  geom_density(aes(dollar_rate_normalized  , fill = "Dollar Currency Rate"), , alpha = .2, data = df_ts) +  
+  scale_fill_manual(name = "Indexes", values = c("Producer Price Index" = "red", "Residency Price Index" = "green", "Dollar Index"="blue"))+
+  labs(title = "Density Comparison for Normalized Values", x="Normalized Values for Datasets", y="Density")+ 
+    theme(
+    legend.position = c(.95, .95),
+    legend.justification = c("right", "top"),
+    legend.box.just = "right",
+    legend.margin = margin(6, 6, 6, 6)
+    )#+ scale_fill_discrete(breaks=c("Dollar Index","Residency Price Index","Producer Price Index"))
 
+library(corrplot)
+df_ts_req=df_ts[,c(2,3,4)]
+colnames(df_ts_req)<- c("Producer Price Index", "Residency Price Index", "Dollar Index")
+ggcorr(data.frame(df_ts_req), method = c("everything", "pearson")) 
 
+M<-cor(data.frame(df_ts_req))
+colnames(M)<- c("Producer Price Index", "Residency Price Index", "Dollar Index")
+rownames(M)<- c("Producer Price Index", "Residency Price Index", "Dollar Index")
+corrplot(M,
+  method = "number",
+  type = "upper" ,# show only upper side
+)
 
-ggplot(df_ts,)
+cor(data.frame(df_ts_req), method = "pearson")
 
-#General looking with 3 different graph
-plot(zoo(df_ts))
-
-
-
-df_ts_req=df_ts[,c(1,2,3)]
-df_ts_req
-#Correlation between the variables
 ggpairs(data.frame(df_ts_req))
 
 
-
-
-
-
-
-#ggplot(price_index, aes(x=Tarih,y=TP.TUFE1YI.T1))+geom_line()+ggtitle("Time vs Price Index(01/2017 to 08/2020)")+
-#  ylab("Price Index")+scale_x_date(date_breaks = "month")+ theme(axis.text.x = element_text(angle = 45,hjust = 1))
